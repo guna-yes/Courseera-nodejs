b64205a66ffb8d04ef412a9c98db5bc25dfe1b27
Your Name <sgunasekharguna@gmail.com>
2020-05-13 22:09:02 +0530

    HTTPS

--- app.js
+++ app.js
@@ -57,6 +57,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(passport.initialize());
 // app.use(passport.session());
 
+// Secure traffic only
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  }
+  else {
+    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
+  }
+});
+
 app.use('/', index);
 app.use('/users', users);
 
