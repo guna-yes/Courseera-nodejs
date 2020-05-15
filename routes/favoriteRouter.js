b96f8338efe4b251b728c2db54d2458216699cc8
Your Name <sgunasekharguna@gmail.com>
2020-05-15 22:03:14 +0530

    OAUth

--- routes/favoriteRouter.js
+++ routes/favoriteRouter.js
@@ -1,34 +1,37 @@
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const mongoose = require('mongoose');
-// const  Favourite= require('../models/favourites');
-// var authenticate = require('../authenticate');
-// const cors = require('./cors');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const  Favourite= require('../models/favourites');
+var authenticate = require('../authenticate');
+const cors = require('./cors');
 
 
-// const favouriteRouter=express.Router();
-// favouriteRouter.use(bodyParser.urlencoded({extended:true}))
+const favouriteRouter=express.Router();
+favouriteRouter.use(bodyParser.urlencoded({extended:true}))
 
-// favouriteRouter("/")
-//     .options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
-//     .get(authenticate.verifyUser,(req,res)=>{
-//         Favourite.find({})
-//         .populate("user")
-//         .populate("favouriteDishes")
-//             .then((dishes) => {
-//                 res.statusCode = 200;
-//                 res.setHeader('Content-Type', 'application/json');
-//                 res.json(dishes);
-//             }, (err) => next(err))
-//             .catch((err) => next(err));
+favouriteRouter("/")
+    .options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
+    .get(authenticate.verifyUser,(req,res)=>{
+        Favourite.find({})
+        .populate("user")
+        .populate("favouriteDishes")
+            .then((dishes) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(dishes);
+            }, (err) => next(err))
+            .catch((err) => next(err));
 
-//     })
-
-
-
-
-
+    })
+    .post(authenticate.verifyUser,(req,res)=>{
+        
+    })
 
 
 
-// module.exports=favouriteRouter;
+
+
+
+
+
+module.exports=favouriteRouter;
